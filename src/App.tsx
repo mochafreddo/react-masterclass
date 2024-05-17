1e75f23406e410c4b8ba12955e20567617d5fe57
Jeongwoo Kim <corc411@gmail.com>
2024-05-17 16:24:00 +0900

    chore: update dependencies and add missing types
    - Added `@types/react-query` to `devDependencies`.
    - Re-added `@types/styled-components` to `devDependencies`.
    - Removed `@types/styled-components` from `dependencies`.

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,24 @@ const H1 = styled.h1`
   color: ${(props) => props.theme.textColor};
 `;
 
+interface DummyProps {
+  text: string;
+  active?: boolean;
+}
+
+function Dummy({ text, active = false }: DummyProps) {
+  return <H1>{text}</H1>;
+}
+
 function App() {
+  const onClick = (event: React.FormEvent<HTMLButtonElement>) => {};
+
   return (
     <Container>
-      <H1>protected</H1>
+      <Dummy active text="hello" />
+      <form>
+        <button onClick={onClick}>Click me</button>
+      </form>
     </Container>
   );
 }
