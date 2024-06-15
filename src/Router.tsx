f8262ecd1bea288ca133146e472766bd4eb19f23
Jeongwoo Kim <corc411@gmail.com>
2024-06-16 01:14:42 +0900

    refactor: update routing and global styles
    - Moved global styles to `GlobalStyles.ts`.
    - Updated `App.tsx` to use `ThemeProvider` and `RouterProvider`.
    - Refactored `Router.tsx` to use `createBrowserRouter`.
    - Updated `Coin.tsx` to use nested routes and `Outlet`.

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,17 +1,29 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 
+import Chart from './routes/Chart';
 import Coin from './routes/Coin';
 import Coins from './routes/Coins';
+import Price from './routes/Price';
 
-function Router() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Coins />} />
-        <Route path="/:coinId" element={<Coin />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Coins />,
+  },
+  {
+    path: '/:coinId',
+    element: <Coin />,
+    children: [
+      {
+        path: 'chart',
+        element: <Chart />,
+      },
+      {
+        path: 'price',
+        element: <Price />,
+      },
+    ],
+  },
+]);
 
-export default Router;
+export default router;
--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,9 +1,7 @@
 import { useEffect, useState } from 'react';
-import { Route, Routes, useLocation, useParams } from 'react-router-dom';
+import { Link, Outlet, useLocation, useParams } from 'react-router-dom';
+import { useMatch } from 'react-router-dom';
 import styled from 'styled-components';
-
-import Chart from './Chart';
-import Price from './Price';
 
 const Title = styled.h1`
   font-size: 48px;
@@ -48,6 +46,28 @@ const OverviewItem = styled.div`
 `;
 const Description = styled.p`
   margin: 20px 0px;
+`;
+
+const Tabs = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  margin: 25px 0px;
+  gap: 10px;
+`;
+
+const Tab = styled.span<{ isActive: boolean }>`
+  text-align: center;
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: 400;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 7px 0px;
+  border-radius: 10px;
+  color: ${(props) =>
+    props.isActive ? props.theme.accentColor : props.theme.textColor};
+  a {
+    display: block;
+  }
 `;
 
 interface RouteState {
@@ -110,14 +130,16 @@ interface PriceData {
 }
 
 function Coin() {
-  const [info, setInfo] = useState<InfoData>();
-  const [loading, setLoading] = useState(true);
-  const [priceInfo, setPriceInfo] = useState<PriceData>();
-
   const { coinId } = useParams<{ coinId: string }>();
-
   const location = useLocation();
   const state = location.state as RouteState;
+
+  const [info, setInfo] = useState<InfoData>();
+  const [priceInfo, setPriceInfo] = useState<PriceData>();
+  const [loading, setLoading] = useState(true);
+
+  const chartMatch = useMatch('/:coinId/chart');
+  const priceMatch = useMatch('/:coinId/price');
 
   useEffect(() => {
     (async () => {
@@ -170,10 +192,17 @@ function Coin() {
               <span>{priceInfo?.max_supply}</span>
             </OverviewItem>
           </Overview>
-          <Routes>
-            <Route path="price" element={<Price />} />
-            <Route path="chart" element={<Chart />} />
-          </Routes>
+
+          <Tabs>
+            <Tab isActive={chartMatch !== null}>
+              <Link to={`/${coinId}/chart`}>Chart</Link>
+            </Tab>
+            <Tab isActive={priceMatch !== null}>
+              <Link to={`/${coinId}/price`}>Price</Link>
+            </Tab>
+          </Tabs>
+
+          <Outlet />
         </>
       )}
     </Container>
