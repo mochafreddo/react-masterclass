6c675a6d2435c2cf48abd51a09514560e3967bcb
Jeongwoo Kim <corc411@gmail.com>
2024-06-18 22:36:05 +0900

    chore: migrate commitlint config to ES module
    - Deleted `commitlint.config.js`.
    - Created `commitlint.config.mjs` with ES module syntax.
    - Added commitlint rules for header max length, type enum, subject case, and subject full stop.

---
+++ commitlint.config.mjs
@@ -0,0 +1,31 @@
+const config = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [2, 'always', 50],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+      ],
+    ],
+    'subject-case': [
+      2,
+      'never',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+    ],
+    'subject-full-stop': [2, 'never', '.'],
+  },
+};
+
+export default config;
