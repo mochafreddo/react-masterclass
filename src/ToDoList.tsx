e8607663cdf04b540ee367f93b3e2e8c0a324715
Jeongwoo Kim <corc411@gmail.com>
2024-06-30 20:43:49 +0900

    feat(ToDoList): implement react-hook-form for form handling
    - Add react-hook-form dependency
    - Replace useState with useForm hook in ToDoList component
    - Simplify form input handling using register function

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,28 +1,15 @@
-import { useState } from 'react'
+import { useForm } from 'react-hook-form'
 
 function ToDoList() {
-  const [toDo, setToDo] = useState('')
-
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event
-    setToDo(value)
-  }
-
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    console.log(toDo)
-  }
+  const { register } = useForm()
 
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={toDo} placeholder="Write a to do" />
+      <form>
+        <input {...register('toDo')} placeholder="Write a to do" />
         <button>Add</button>
       </form>
     </div>
   )
 }
-
 export default ToDoList
