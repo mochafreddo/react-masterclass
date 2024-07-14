3ad38bbbe12a84a78ab37be84c7c852aa79cb364
Jeongwoo Kim <corc411@gmail.com>
2024-07-14 22:55:19 +0900

    feat(todo): implement Recoil state management for ToDo list
    - Add Recoil dependency
    - Wrap App component with RecoilRoot
    - Create toDoState atom for managing ToDo items
    - Update ToDoList component to use Recoil state
    - Implement adding new ToDo items to the list
    - Display ToDo items in an unordered list

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
+import { RecoilRoot } from 'recoil'
 import GlobalStyle from './GlobalStyle'
-import ToDoList from './ToDoList'
+import ToDoList from './components/ToDoList'
 
 function App() {
   return (
-    <>
+    <RecoilRoot>
       <GlobalStyle />
       <ToDoList />
-    </>
+    </RecoilRoot>
   )
 }
 
