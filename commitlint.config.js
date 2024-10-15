d7e6a610db694c4cfb7d9c0c25ff2f6e746e2317
Jeongwoo Kim <corc411@gmail.com>
2024-10-15 20:38:16 +0900

    🎨 Enhance commit linting configuration
    This commit improves the structure and formatting of our commit messages by:
    
    1. Adding a custom parser preset to support gitmoji in commit messages
    2. Expanding the list of allowed commit types
    3. Relaxing rules for subject case and full stop
    
    The changes will allow for more expressive and standardized commit messages, including the use of emojis at the beginning of the commit message.

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,30 @@
 export default {
   extends: ['@commitlint/config-conventional'],
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^(?::\w+:\s)?(\w+)(?:\((.+)\))?:\s(.+)/,
+      headerCorrespondence: ['type', 'scope', 'subject'],
+    },
+  },
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'build',
+        'ci',
+        'chore',
+        'revert',
+      ],
+    ],
+    'subject-full-stop': [0, 'never'],
+    'subject-case': [0, 'never'],
+  },
 }
