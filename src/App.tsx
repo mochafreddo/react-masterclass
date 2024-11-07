27adadc5633ec85eb9f5e5085de14fc2137d4e5f
Jeongwoo Kim <corc411@gmail.com>
2024-11-07 23:16:21 +0900

    :sparkles: Add routing and header components
    - Integrated `react-router-dom` for routing functionality.
    - Implemented `Header` component with styled-components for navigation.
    - Added `Home`, `Search`, and `Tv` route components.
    - Updated `App.tsx` to include routes and header.
    - Updated `package.json` to include `react-router-dom` dependency.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
-const App = () => {
-  return null;
-};
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
+import Header from './Components/Header';
+import Home from './Routes/Home';
+import Search from './Routes/Search';
+import Tv from './Routes/Tv';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tv" element={<Tv />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
---
+++ src/Routes/Home.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div style={{ backgroundColor: 'whitesmoke', height: '200vh' }}></div>;
+}
