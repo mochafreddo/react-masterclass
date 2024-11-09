640cff0454fd2b2463d050b242deec7a6addc4e0
Jeongwoo Kim <corc411@gmail.com>
2024-11-10 00:20:00 +0900

    ♻️ refactor: update router implementation with future flag

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Header from './Components/Header';
 import Home from './Routes/Home';
@@ -7,14 +7,14 @@ import Tv from './Routes/Tv';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter future={{ v7_startTransition: true }}>
       <Header />
       <Routes>
         <Route path="/tv" element={<Tv />} />
         <Route path="/search" element={<Search />} />
         <Route path="/" element={<Home />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
